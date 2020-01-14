53012b5275197e948b38bacc28cc2dc2b961889b
wenglin1 <wenglin@buffalo.edu>
2020-01-14 10:14:22 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
-require 'pry'
-
+# require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/[\.!?] /).count
   end
 end
+
+# str = String.new
+# str = "Hi! This is a sentence. Is it? Cool."
+# binding.pry
+# "hi"
